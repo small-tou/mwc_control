9a47a3f75bdeb84ad1c1a3d21e2e83af3e340003
sunxinyu <sunxinyu@souche.com>
2015-12-16 16:53:10 +0800

    hello

---
+++ mwc_control.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+// 左边油门（上下运动）/方向 （水平旋转） ，右边副翼（左右俯冲）/升降 （前后俯冲）
+#define YOUMEN 2;
+#define FANGXIANG 3;
+#define FUYI 4;
+#define SHENGJIANG 5;
+
+#define min_pwm 1000
+#define max_pwm 2000
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void youmen(){
+  
+}
+void fangxiang(){
+  
+}
+void fuyi(){
+  
+}
+void shengjiang(){
+  
+}
+
