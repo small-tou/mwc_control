6af46dd997d03571d88773c1bc0d17c4822eea02
sunxinyu <sunxinyu@souche.com>
2015-12-16 19:00:45 +0800

    用servo输出四通道波形，频率50HZ，利用cpu空余时间读取蓝牙信号

--- mwc_control.ino
+++ mwc_control.ino
@@ -6,32 +6,64 @@
 #define FUYI 4;
 #define SHENGJIANG 5;
 
-#define min_pwm 1000
-#define max_pwm 2000
+#define MIN_PWM 1000
+#define MAX_PWM 2000
 
+#define ONE_CHANNEL_TIME 2500 //一个通道给的占空时间
+#define TOTAL_CHANNEL_TIME 20000 //定义频率
 
+Servo YOUMEN_SERVO;
+Servo FANGXIANG_SERVO;
+Servo FUYI_SERVO;
+Servo SHENGJIANG_SERVO;
+//记录每个通道的值，会一直使用，可以用蓝牙修改
+int youmen_v = 0;
+int fangxiang_v = 0;
+int fuyi_v = 0;
+int shengjiang_v = 0;
 
+int time ;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  //初始化servo
+  YOUMEN_SERVO.attach(YOUMEN);
+  FANGXIANG_SERVO.attach(FANGXIANG);
+  FUYI_SERVO.attach(FUYI);
+  SHENGJIANG_SERVO.attach(SHENGJIANG);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
-
-void youmen(){
+  time = micros(); //记录当前微秒，每次loop执行不足TOTAL_CHANNEL_TIME的话，就delay剩下的时间
+  youmen();
+  fangxiang();
+  fuyi();
+  shengjiang();
   
+}
+/**
+ * value 从0 - 100
+ */
+void youmen(){
+  int v = map(youmen_v, 0, 100, MIN_PWM, MAX_PWM); 
+  YOUMEN_SERVO.writeMicroseconds(v);
+  delayMicroseconds(ONE_CHANNEL_TIME-v);
 }
 void fangxiang(){
-  
+  int v = map(fangxiang_v, 0, 100, MIN_PWM, MAX_PWM); 
+  FANGXIANG_SERVO.writeMicroseconds(v);
+  delayMicroseconds(ONE_CHANNEL_TIME-v);
 }
 void fuyi(){
-  
+  int v = map(fuyi_v, 0, 100, MIN_PWM, MAX_PWM); 
+  FUYI_SERVO.writeMicroseconds(v);
+  delayMicroseconds(ONE_CHANNEL_TIME-v);
 }
 void shengjiang(){
-  
+  int v = map(shengjiang_v, 0, 100, MIN_PWM, MAX_PWM); 
+  SHENGJIANG_SERVO.writeMicroseconds(v);
+  delayMicroseconds(ONE_CHANNEL_TIME-v);
 }
+
+
 
